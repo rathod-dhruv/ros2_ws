f2c6ab7e1d80c84baab5c391470a7dbbbe1c2d5b
Dhruv <52994515+rathod-dhruv@users.noreply.github.com>
2026-03-07 22:40:57 -0600

    Service Client Created - CPP package

--- src/my_cpp_pkg/src/number_counter.cpp
+++ src/my_cpp_pkg/src/number_counter.cpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/int64.hpp"
+#include "example_interfaces/srv/set_bool.hpp"
 
 class NumberCounterNode : public rclcpp::Node
 {
@@ -12,7 +13,11 @@ class NumberCounterNode : public rclcpp::Node
                 "number", 10, std::bind(&NumberCounterNode::callbackNumber, this, std::placeholders::_1)
             );
             publisher_ = this->create_publisher<example_interfaces::msg::Int64>("number_counter", 10);
+            service_ = this->create_service<example_interfaces::srv::SetBool>(
+                "reset_counter", std::bind(&NumberCounterNode::callbackResetCounter, this, std::placeholders::_1, std::placeholders::_2)
+            );
            
+            
         }
     
     private:
@@ -31,8 +36,28 @@ class NumberCounterNode : public rclcpp::Node
             RCLCPP_INFO(this->get_logger(), "Published counter: %ld", (long int)message.data);
         }
         
+        void callbackResetCounter(const example_interfaces::srv::SetBool::Request::SharedPtr request,
+                                  const example_interfaces::srv::SetBool::Response::SharedPtr response)
+        {
+            if (request->data) // If the request is to reset the counter
+            {
+                counter_ = 0; // Reset the counter
+                response->success = true;
+                response->message = "Counter has been reset.";
+                RCLCPP_INFO(this->get_logger(), "Counter reset requested. Counter is now: %ld", (long int)counter_);
+            }
+            else
+            {
+                response->success = false;
+                response->message = "Counter reset not requested.";
+                RCLCPP_INFO(this->get_logger(), "Counter reset not requested. Counter remains: %ld", (long int)counter_);
+            }
+        }
+
         rclcpp::Subscription<example_interfaces::msg::Int64>::SharedPtr subscription_;
         rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr publisher_;
+        rclcpp::Service<example_interfaces::srv::SetBool>::SharedPtr service_;
+
         int counter_ = 0;
 };
 
