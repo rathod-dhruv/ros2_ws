f19019740cbe3cadaf84e5e1afbdb5dc0664a37d
Dhruv <52994515+rathod-dhruv@users.noreply.github.com>
2026-03-09 21:11:33 -0500

    Working on Parameter

--- src/my_py_pkg/my_py_pkg/number_publisher.py
+++ src/my_py_pkg/my_py_pkg/number_publisher.py
@@ -6,9 +6,12 @@ from example_interfaces.msg import Int64
 class NumberPublisherNode(Node):
     def __init__(self):
         super().__init__("number_publisher")
-        self.number_ = 2
+        self.declare_parameter("number", 2)
+        self.declare_parameter("timer_period", 1.0)
+
+        self.number_ = self.get_parameter("number").value
         self.number_publisher_ = self.create_publisher(Int64, "number", 10)
-        self.number_timer_ = self.create_timer(1.0, self.publish_number)
+        self.number_timer_ = self.create_timer(self.get_parameter("timer_period").value, self.publish_number)
         self.get_logger().info("Number publisher has been started.")
 
     def publish_number(self):
